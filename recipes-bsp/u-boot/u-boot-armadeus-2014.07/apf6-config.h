e89377d2b7d6c5317ded6651b2d1a71bcb0ec285
Gwenhael Goavec-Merou <gwenhael.goavec-merou@trabucayre.com>
2017-03-14 11:46:32 +0100

    u-boot-armadeus-2014.07: update apf6-config.h

--- recipes-bsp/u-boot/u-boot-armadeus-2014.07/apf6-config.h
+++ recipes-bsp/u-boot/u-boot-armadeus-2014.07/apf6-config.h
@@ -16,7 +16,7 @@
 #define CONFIG_SYS_GENERIC_BOARD
 
 #define CONFIG_VERSION_VARIABLE
-#define CONFIG_ENV_VERSION	5
+#define CONFIG_ENV_VERSION	21
 #define CONFIG_BOARD_NAME	apf6
 
 /* SPL */
@@ -84,6 +84,7 @@
 #define CONFIG_SYS_MEMTEST_START	0x10000000
 #define CONFIG_SYS_MEMTEST_END		(CONFIG_SYS_MEMTEST_START + 500 * SZ_1M)
 #define CONFIG_LOADADDR			0x12000000
+#define CONFIG_STANDALONE_LOAD_ADDR	CONFIG_LOADADDR
 
 /* MMC Configuration */
 #define CONFIG_SUPPORT_EMMC_BOOT
@@ -133,6 +134,7 @@
 #define CONFIG_MXC_USB_PORTSC		(PORT_PTS_UTMI | PORT_PTS_PTW)
 #define CONFIG_MXC_USB_FLAGS		0
 #define CONFIG_EHCI_HCD_INIT_AFTER_RESET
+#define CONFIG_USB_KEYBOARD
 /* Gadget part */
 #define CONFIG_CI_UDC
 #define CONFIG_USB_GADGET
@@ -169,7 +171,7 @@
 #define CONFIG_HOSTNAME         CONFIG_BOARD_NAME
 #define CONFIG_ROOTPATH         "/tftpboot/" __stringify(CONFIG_BOARD_NAME) "-root"
 
-#define CONFIG_PREBOOT		"run check_env;"
+#define CONFIG_PREBOOT		"run checknload_firmware check_env;"
 
 #define ACFG_CONSOLE_DEV        ttymxc3
 #define CONFIG_SYS_AUTOLOAD	"no"
@@ -180,9 +182,10 @@
 	"env_version="          __stringify(CONFIG_ENV_VERSION)         "\0"			\
 	"consoledev="           __stringify(ACFG_CONSOLE_DEV)           "\0" 			\
 	"board_name="           __stringify(CONFIG_BOARD_NAME)          "\0" 			\
-	"fdt_high=0xffffffff\0" 								\
-	"initrd_high=0xffffffff\0" 								\
 	"fdt_addr=0x18000000\0" 								\
+	"fdt_high=0xffffffff\0" 								\
+	"fdt_name="           __stringify(CONFIG_BOARD_NAME)          "dev\0" 			\
+	"initrd_high=0xffffffff\0" 								\
 	"ip_dyn=yes\0" 										\
 	"stdin=serial\0"									\
 	"stout=serial\0"									\
@@ -191,6 +194,9 @@
 	"mmcpart=1\0" 										\
 	"mmcroot=/dev/mmcblk2p2 ro\0" 								\
 	"mmcrootfstype=ext4 rootwait\0" 							\
+	"firmware_autoload=0\0"									\
+	"firmware_path=/lib/firmware/fpga/firmware.periph.rbf\0"				\
+	"kernelimg="           __stringify(CONFIG_BOARD_NAME)          "-linux.bin\0" 		\
 	"check_env=if test -n ${flash_env_version}; "						\
 		"then env default env_version; "						\
 		"else env set flash_env_version ${env_version}; env save; "			\
@@ -224,20 +230,34 @@
 		"fi;\0" 									\
 	"update_uboot=run download_uboot_spl flash_uboot_spl " 					\
 		"download_uboot_img flash_uboot_img\0" 						\
-	"download_kernel=tftpboot ${loadaddr} ${board_name}-linux.bin\0" 			\
+	"download_kernel=tftpboot ${loadaddr} ${kernelimg}\0"		 			\
 	"flash_kernel=" 									\
-		"if ext4write mmc ${mmcdev}:${mmcpart} ${loadaddr} /${board_name}-linux.bin ${filesize}; then "	\
-		      "echo Flashing of kernel succeed; "                      			\
-		      "else echo Flashing of kernel failed; "                  			\
+		"if ext4write mmc ${mmcdev}:${mmcpart} ${loadaddr} /${kernelimg} ${filesize}; then "	\
+		      "echo kernel update in Boot partition succeed; "                      			\
+		      "else echo kernel update in Boot partition failed; "                  			\
 		"fi;\0"						               			\
 	"update_kernel=run download_kernel flash_kernel\0" 					\
-	"download_dtb=tftpboot ${loadaddr} ${fdt_file}\0" 					\
+	"download_dtb=tftpboot ${fdt_addr} ${cpu_type}-${fdt_name}.dtb\0"			\
 	"flash_dtb=" 										\
-		"if ext4write mmc ${mmcdev}:${mmcpart} ${loadaddr} /dtbs/${fdt_file} ${filesize}; then " 	\
-		      "echo Flashing of dtb succeed; "                      			\
-		      "else echo Flashing of dtb failed; "                  			\
+		"if ext4write mmc ${mmcdev}:${mmcpart} ${fdt_addr} /${cpu_type}-${fdt_name}.dtb ${filesize}; then " \
+		      "echo dtb update in Boot partition succeed; "                      	\
+		      "else echo dtb update in Boot partition failed; "                  	\
 		"fi;\0" 									\
 	"update_dtb=run download_dtb flash_dtb\0" 						\
+	"download_boot=tftpboot ${loadaddr} ${board_name}-boot.ext4\0"				\
+	"flash_boot=" 									\
+		"if mmc dev 0 0; then "								\
+			"setexpr nbblocks ${filesize} / 0x200; " 				\
+			"setexpr nbblocks ${nbblocks} + 1; " 					\
+			"if mmc write ${loadaddr} 0x800 ${nbblocks}; then " 			\
+				"echo Flashing of boot image succeed; " 			\
+			"else echo Flashing of boot image failed; " 				\
+			"fi; "									\
+		"fi;\0" 									\
+	"update_boot=run download_boot flash_boot\0" 					\
+	"erase_rootfs=echo Erasing 10 first MB of eMMC rootfs partition: ; "			\
+		"mw ${loadaddr} 0 0xa00000 && mmc dev 0 0 && "					\
+		"mmc write ${loadaddr} 0x18800 0x5000\0"						\
 	"download_rootfs=tftpboot ${loadaddr} ${board_name}-rootfs.ext4\0" 			\
 	"flash_rootfs=" 									\
 		"if mmc dev 0 0; then "								\
@@ -249,29 +269,64 @@
 			"fi; "									\
 		"fi;\0" 									\
 	"update_rootfs=run download_rootfs flash_rootfs\0" 					\
-	"update_all=run update_kernel update_rootfs update_dtb update_uboot\0"				\
-	"loadzimage=load mmc ${mmcdev}:${mmcpart} ${loadaddr} ${board_name}-linux.bin\0" 	\
-	"loadfdt=load mmc ${mmcdev}:${mmcpart} ${fdt_addr} /dtbs/${fdt_file}\0" 		\
+	"download_userdata=tftpboot ${loadaddr} ${board_name}-user_data.ext4\0" 		\
+	"flash_userdata=" 									\
+		"if mmc dev 0 0; then "								\
+			"setexpr nbblocks ${filesize} / 0x200; " 				\
+			"setexpr nbblocks ${nbblocks} + 1; " 					\
+			"if mmc write ${loadaddr} 0 ${nbblocks}; then " 			\
+				"echo Flashing of user_data image succeed; " 			\
+			"else echo Flashing of user_data image failed; " 			\
+			"fi; "									\
+		"fi;\0" 									\
+	"update_userdata=run download_userdata flash_userdata; mmc rescan\0"			\
+	"erase_userdata="									\
+		"if mmc dev 0 0; then "								\
+			"echo Erasing eMMC User Data partition, no way out...; "		\
+			"mw ${loadaddr} 0 0x200000; "						\
+			"mmc write ${loadaddr} 0 0x1000; "					\
+			"mmc write ${loadaddr} 0x800 0x1000; "					\
+			"mmc write ${loadaddr} 0x18800 0x1000; "					\
+		"fi;"										\
+		"mmc rescan\0"									\
+	"update_all=run update_kernel update_rootfs update_dtb update_uboot\0"			\
+	"load_firmware=ext2load mmc 0:2 ${loadaddr} ${firmware_path} && "			\
+		"fpga load 0 ${loadaddr} ${filesize}\0"						\
+	"checknload_firmware=if test ${firmware_autoload} = 1;"					\
+		"then run load_firmware; else exit; fi;\0"					\
+	"mmcloadzimage=load mmc ${mmcdev}:${mmcpart} ${loadaddr} ${mmcbootdir}/${kernelimg}\0" 	\
+	"mmcloadfdt=load mmc ${mmcdev}:${mmcpart} ${fdt_addr} "					\
+		"${mmcbootdir}/${mmcdtbdir}/${cpu_type}-${fdt_name}.dtb || "			\
+		"echo Please update your eMMC Boot partition\0" 				\
+	"initargs=setenv bootargs console=${consoledev},${baudrate} ${extrabootargs}\0"		\
 	"addipargs=setenv bootargs ${bootargs} ip=${ipaddr}:${serverip}:"			\
 		"${gatewayip}:${netmask}:${hostname}:eth0:off\0"				\
 	"addmmcargs=setenv bootargs ${bootargs} root=${mmcroot} " 				\
 		"rootfstype=${mmcrootfstype}\0" 						\
-	"emmcboot=setenv bootargs console=${consoledev},${baudrate} ${extrabootargs}; "		\
-		"run addmmcargs; "								\
-		"run loadzimage && run loadfdt && bootz ${loadaddr} - ${fdt_addr};\0"		\
+	"genmmcboot=run initargs; run addmmcargs; "						\
+		"run mmcloadzimage && run mmcloadfdt && bootz ${loadaddr} - ${fdt_addr};\0"	\
+	"mmcboot=setenv mmcdev 1; setenv mmcpart 1; setenv mmcbootdir /boot; "			\
+		"setenv mmcroot /dev/mmcblk1p1 ro; "						\
+		"run genmmcboot;\0"								\
+	"emmcboot=setenv mmcdev 0; setenv mmcpart 1; setenv mmcbootdir; "			\
+		"setenv mmcroot /dev/mmcblk2p2 ro; "						\
+		"run genmmcboot;\0"								\
+	"addramargs=setenv bootargs ${bootargs} root=/dev/ram rw\0"				\
+	"ramboot=run initargs; run addramargs; "						\
+		"run download_kernel && run download_dtb && "					\
+		"bootz ${loadaddr} - ${fdt_addr};\0"						\
 	"addnfsargs=setenv bootargs ${bootargs} root=/dev/nfs rw "				\
 		"nfsroot=${serverip}:${rootpath}\0"						\
-	"nfsboot=setenv bootargs console=${consoledev},${baudrate} ${extrabootargs}; "		\
-		"run addnfsargs addipargs; "							\
-		"nfs ${loadaddr} ${serverip}:${rootpath}/boot/${board_name}-linux.bin && "	\
-		"nfs ${fdt_addr} ${serverip}:${rootpath}/boot/dtbs/${fdt_file} && "		\
+	"nfsboot=run initargs; run addnfsargs addipargs; "					\
+		"nfs ${loadaddr} ${serverip}:${rootpath}/boot/${kernelimg} && "			\
+		"nfs ${fdt_addr} ${serverip}:${rootpath}/boot/${cpu_type}-${fdt_name}.dtb && "	\
 		"bootz ${loadaddr} - ${fdt_addr};\0"
 
 /* Miscellaneous configurable options */
 #define CONFIG_AUTO_COMPLETE
 #define CONFIG_CMDLINE_EDITING
 #define CONFIG_CONSOLE_MUX
-#define CONFIG_SYS_CBSIZE		256
+#define CONFIG_SYS_CBSIZE		384	/* Console I/O Buffer Size */
 #define CONFIG_SYS_CONSOLE_IS_IN_ENV
 #define CONFIG_SYS_HUSH_PARSER
 #define CONFIG_SYS_LONGHELP
